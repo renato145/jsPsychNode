6b1d7b29dd82b89b46b42208e802935fdca11a51
Renato Hermoza <renato145@hotmail.com>
2016-01-11 18:20:56 -0500

    initial commit

---
+++ app/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
+	'url' : 'mongodb://mongo:27017/test'
+}
+
+//MongoClient = require('mongodb').MongoClient,
+//mongoUrl = util.format('mongodb://mongo:%s/demo', process.env.MONGO_PORT);
